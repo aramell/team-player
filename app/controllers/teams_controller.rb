e15581f0616d434b005abed2e3a28e058bc1ce95
Andrew <github email address>
2017-10-13 14:20:18 -0400

    added new team and team show

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -2,20 +2,33 @@ class TeamsController < ApplicationController
 
 
   def index
+
     @teams = Team.all
 
   end 
   def new
     # binding.pry
-    @teams = Team.new
+    @team = Team.new
 
   end
   def create
-    binding.pry
+    @team = Team.new(team_params)
+    if @team.save
+      redirect_to team_path(@team)
+    else
+      redirect_to new_team_path
+    end
 
   end
   def show
+    # binding.pry
     @team = Team.find_by(:id => params[:id])
+
+  end
+
+  private
+  def team_params
+    params.require(:team).permit(:name, :positions, :game_id, :user_id)
 
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users, :controllers => { :omniauth_callbacks => "callbacks" }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root to: 'team#new'
+  root to: 'teams#new'
   resources :teams
   resources :games
   
