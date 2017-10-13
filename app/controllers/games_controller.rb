c9752ee2efbe3c36bb36fe53044b98f7ab686285
Andrew <github email address>
2017-10-13 14:36:19 -0400

    new game and game show

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,5 +1,30 @@
 class GamesController < ApplicationController
   def index
+    @games = Game.all
+  end
+  
+  def new
+    @game = Game.new
+  end
+
+  def create
+    @game = Game.new(game_params)
+    binding.pry
+    if @game.save
+      redirect_to game_path(@game)
+    else
+      redirect_to new_game_path
+    end
+
+  end
+
+  def show
+    @team = Team.find_by(:id => params[:id])
+    
+  end
+  private
+  def game_params
+    params.require(:game).permit(:game_date, :game_time, :location, :team_id, :user_id)
 
   end
 end
