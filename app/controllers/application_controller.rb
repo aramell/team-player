615baf636000df399bbffe45be0e2a983419a689
Andrew <github email address>
2017-10-14 10:54:29 -0400

    added home page with signin/signout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,6 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   
   def index
-
+    
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users, :controllers => { :omniauth_callbacks => "callbacks" }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root to: 'users#show'
+  root to: 'application#index'
   resources :teams
   resources :games
   resources :users
