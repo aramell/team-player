e0f1d016dd8c4bd46a781f87d74d6a7f63d37f5b
Andrew <andrew.ramell@gmail.com>
2017-10-17 20:28:50 -0400

    removed wedding site

--- app/models/team.rb
+++ app/models/team.rb
@@ -7,11 +7,9 @@ class Team < ApplicationRecord
 
   def sports_attributes=(sports_hash)
     sports_hash.each do |i, sport|
-      binding.pry
            new_sport = Sport.find_or_create_by(:name => sport[:name])
            
           self.sports.build(:name => new_sport.name)
-          binding.pry
     end
   end
 
